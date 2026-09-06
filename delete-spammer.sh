c41189a83c3e5f75c91a40fad5b171015d660a5a
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-09-07 00:06:49 +0900

    fix delete spammer

--- delete-spammer.sh
+++ delete-spammer.sh
@@ -2,14 +2,14 @@
 
 username="$(echo "$1" | xargs)"
 echo "Trying to delete $username"
-psql="docker exec -ti root_mattermost_db_1 psql -U mmuser -d mattermost"
+psql="docker exec -i btcpayserver-infra-mattermost_db-1 psql -U mmuser -d mattermost"
 userid=$($psql -qtAX -c "SELECT id FROM users WHERE username='$username'")
 if [[ $userid ]]; then
 echo "User found deleting..."
 $psql -c "DELETE FROM users WHERE username='$username';"
 $psql -c "DELETE FROM posts WHERE userid='$userid';"
 $psql -c "DELETE FROM channelmembers WHERE userid='$userid';"
-find /var/lib/docker/volumes/root_mm-data/_data -type d | grep $userid | tr "\n" "\0" | xargs -0 rm -rf
+find /var/lib/docker/volumes/btcpayserver-infra_mm-data/_data -type d | grep $userid | tr "\n" "\0" | xargs -0 rm -rf
 $psql -c "DELETE FROM sessions WHERE userid='$userid';"
 $psql -c "DELETE FROM fileinfo WHERE creatorid='$userid';"
 else
